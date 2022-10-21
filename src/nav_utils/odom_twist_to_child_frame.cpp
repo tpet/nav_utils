182fb94a3a4700e2023dacb3639a02249d097877
Martin Pecka <peckama2@fel.cvut.cz>
2022-10-21 21:36:47 +0200

    Simplify odom_twist_to_child_frame and fix transform direction bug.

--- src/nav_utils/odom_twist_to_child_frame.cpp
+++ src/nav_utils/odom_twist_to_child_frame.cpp
@@ -3,9 +3,8 @@
 #include <nav_msgs/Odometry.h>
 #include <nav_utils/odom_twist_to_child_frame.h>
 #include <ros/ros.h>
-#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Vector3.h>
-#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace nav_utils
@@ -18,34 +17,24 @@ OdometryTwistToChildFrame::OdometryTwistToChildFrame(ros::NodeHandle &nh, ros::N
 
 void OdometryTwistToChildFrame::processOdometry(const nav_msgs::Odometry &odom)
 {
-  tf2::Quaternion parent_to_child_rot;
-  tf2::convert(odom.pose.pose.orientation, parent_to_child_rot);
+  tf2::Transform child_to_parent_tf;
+  tf2::convert(odom.pose.pose, child_to_parent_tf);
+  const auto parent_to_child_tf = child_to_parent_tf.inverse();
   
   nav_msgs::Odometry odom_out = odom;
   
   // Transform linear twist (just rotate the vector, do not apply translation!)
   tf2::Vector3 linear_twist;
   tf2::convert(odom.twist.twist.linear, linear_twist);
-  tf2::convert(tf2::quatRotate(parent_to_child_rot, linear_twist), odom_out.twist.twist.linear);
+  tf2::convert(parent_to_child_tf.getBasis() * linear_twist, odom_out.twist.twist.linear);
 
   // Transform angular velocities
   tf2::Vector3 angular_twist;
   tf2::convert(odom.twist.twist.angular, angular_twist);
-  tf2::convert(tf2::quatRotate(parent_to_child_rot, angular_twist), odom_out.twist.twist.angular);
+  tf2::convert(parent_to_child_tf.getBasis() * angular_twist, odom_out.twist.twist.angular);
   
   // Transform the covariance
-  Eigen::Quaterniond eigen_rot_quat;
-  tf2::fromMsg(odom.pose.pose.orientation, eigen_rot_quat);
-  Eigen::Matrix3d eigen_rot = eigen_rot_quat.toRotationMatrix();
-
-  Eigen::Matrix<double, 6, 6> rot6d;
-  rot6d.topLeftCorner<3, 3>() = eigen_rot;
-  rot6d.bottomRightCorner<3, 3>() = eigen_rot;
-  rot6d.topRightCorner<3, 3>() = Eigen::Matrix3d::Zero();
-  rot6d.bottomLeftCorner<3, 3>() = Eigen::Matrix3d::Zero();
-
-  Eigen::Map<Eigen::Matrix<double, 6, 6, Eigen::RowMajor>> cov(odom_out.twist.covariance.data());
-  cov = rot6d * cov * rot6d.transpose();
+  odom_out.twist.covariance = tf2::transformCovariance(odom_out.twist.covariance, parent_to_child_tf);
   
   odom_out_pub_.publish(odom_out);
 }
